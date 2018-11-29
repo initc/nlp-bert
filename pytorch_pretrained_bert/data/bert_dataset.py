86fcba18188ba072892ce432da1443ce370deb11
initc mac pro <516843958@qq.com>
2018-11-29 23:32:13 +0800

    add msmacor data iter

---
+++ pytorch_pretrained_bert/data/bert_dataset.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+import pickle
+
+from pytorch_pretrained_bert.data.datasets import get_batch
+
+import os
+
+class MSmarco_iterator():
+
+    def __init__(self, args, tokenizer, batch_size=1, world_size=4, name="msmarco_train.pk"):
+        
+        self.pad_idx = tokenizer.pad()
+        self.cls_idx = tokenizer.cls()
+        self.sep_idx = tokenizer.sep()
+        self.max_query_len = args.max_query_tokens
+        self.max_passage_len = args.max_passage_tokens
+        self.batch_size = batch_size
+        self.world_size = world_size
+        self.dataset = pickle.load(open(os.path.join(args.data, name), "rb"))
+
+    def __iter__(self):
+        return get_batch(self.dataset, self.pad_idx, self.cls_idx, self.sep_idx, self.max_query_len, self.max_passage_len, self.batch_size, self.world_size)
+
+    def __len__(self):
+        return self.dataset["query_ids"].__len__()
---
+++ pytorch_pretrained_bert/data/datasets.py
@@ -0,0 +1,146 @@
+import os
+import csv
+import json
+import numpy as np
+import pickle
+import torch
+import pdb
+
+from tqdm import tqdm
+
+def msmarco_tokenizer(tokenizer, querys, passages, targets):
+    query_ids = []
+    for query in querys:
+        tokens = tokenizer.tokenize(query.strip())
+        query_ids.append(tokenizer.convert_tokens_to_ids(tokens))
+    passage_ids = [[] for _ in range(len(passages))]
+    for idx in range(len(passages)):
+        for passage in passages[idx]:
+            tokens = tokenizer.tokenize(passage)
+            passage_ids[idx].append(tokenizer.convert_tokens_to_ids(tokens))
+    return {"query_ids":query_ids, "passage_ids":passage_ids, "targets":targets}
+
+    
+
+def _msmarco(path):
+    with open(path) as f:
+        lines = f.readlines()[:1000]
+        ps = [[] for _ in range(10)]
+        qs = []
+        ts = []
+        for line in lines:
+            passages, query, target = line.strip().split('\t')
+            qs.append(query)
+            ts.append(list(json.loads(target)))
+            length = len(passages.split('@@'))
+            for idx, passage in enumerate(passages.split('@@')):
+                ps[idx].append(passage)
+    return qs, ps, ts
+
+def make_msmarco(args, tokenizer):
+    train_queries, train_passages, train_targets = _msmarco(os.path.join(args.origin_data, 'train_processed_selector.txt'))
+    train_data = msmarco_tokenizer(tokenizer, train_queries, train_passages, train_targets)
+    dev_queries, dev_passages, dev_targets = _msmarco(os.path.join(args.origin_data, 'dev_processed_selector.txt'))
+    dev_data = msmarco_tokenizer(tokenizer, dev_queries, dev_passages, dev_targets)
+    with open(os.path.join(args.data, "msmarco_train.pk"), "wb") as f:
+        pickle.dump(train_data, f)
+    with open(os.path.join(args.data, "msmarco_dev.pk"), "wb") as f:
+        pickle.dump(dev_data, f)
+
+
+
+def _batch(data, pad_idx, cls_idx, sep_idx, batch_size, max_query_len, max_passage_len, query_first=True):
+
+    def truncation(queries, passages):
+        b_size = len(queries)
+        query_truncated = 0
+        passage_truncated = 0
+        for idx in range(b_size):
+            if len(queries[idx]) > max_query_len:
+                query_truncated += 1
+                queries[idx] = queries[idx][:max_query_len]
+        for idx in range(len(passages)):
+            for idy in range(b_size):
+                if len(passages[idx][idy]) > max_passage_len:
+                    passage_truncated += 1
+                    passages[idx][idy] = passages[idx][idy][:max_passage_len]
+        # print("| query truncated count {}".format(query_truncated))
+        # print("| passage truncated count {}".format(passage_truncated))
+        return queries, passages
+
+    def max_len(queries, passages):
+        num_exs = len(queries)
+        lengths = []
+        for i in range(num_exs):
+            lengths.append(len(queries[i]) + max([len(passages[j][i]) for j in range(10)]))
+        return max(lengths)
+
+    def sort_by_length(queries, passages, target):
+        num_exs = len(queries)
+        lengths = []
+        for i in range(num_exs):
+            lengths.append(len(queries[i]) + max([len(passages[j][i]) for j in range(10)]))
+        lengths = np.array(lengths)
+        indices = np.argsort(lengths)
+        queries = list(np.array(queries)[indices])
+        for i in range(10):
+            passages[i] = list(np.array(passages[i])[indices])
+        target = list(np.array(target)[indices])
+        return queries, passages, target
+
+    queries, passages = truncation(data["query_ids"], data["passage_ids"])
+    # -----
+    query_ids, passage_ids, targets = sort_by_length(queries, passages, data["targets"])
+    batch_count = len(query_ids)//batch_size
+    for idx in range(batch_count):
+        begin = idx*batch_size
+        end = begin + batch_size
+        # for save memory
+        passage_for_len = []
+        for i in range(len(passage_ids)):
+            passage_for_len.append(passage_ids[i][begin:end])
+        max_time_step = max_len(query_ids[begin:end], passage_for_len)
+        # pdb.set_trace()
+        query_tmp = np.array(query_ids[begin:end])
+        passage_tmp = np.array(passage_for_len)
+
+        input_tensor = np.zeros((batch_size, 10, max_time_step+3), dtype=np.int32)
+        input_tensor.fill(pad_idx)
+        input_type = np.zeros((batch_size, 10, max_time_step+3), dtype=np.int32)
+        input_type.fill(pad_idx)
+        input_mask = np.zeros((batch_size, 10, max_time_step+3), dtype=np.int32)
+        input_mask.fill(0)
+
+        input_tensor[:, :, 0] = cls_idx
+        input_type[:, :, 0] = 0
+        input_mask[:, :, 0] = 1
+
+        for idx_batch in range(batch_size):
+            query_len = len(query_ids[begin+idx_batch])
+            if query_first:
+                input_tensor[idx_batch, :, 1:query_len+1] = query_tmp[idx_batch]
+                input_type[idx_batch, :, 1:query_len+2] = 0
+                input_mask[idx_batch, :, 1:query_len+2] = 1
+                input_tensor[idx_batch, :, query_len+1] = sep_idx
+
+                for idy in range(len(passage_ids)):
+                    passage_len = len(passage_ids[idy][begin+idx_batch])
+
+                    input_tensor[idx_batch, idy, query_len+2:query_len+passage_len+2] = passage_tmp[idy, idx_batch]
+                    input_type[idx_batch, idy, query_len+2:query_len+passage_len+3] = 1
+                    input_mask[idx_batch, idy, query_len+2:query_len+passage_len+3] = 1
+                    input_tensor[idx_batch, idy, query_len+passage_len+2] = sep_idx
+            else:
+                pass
+        targets = torch.FloatTensor(targets[begin:end])
+        yield {"input_ids":torch.LongTensor(input_tensor), "token_type_ids":torch.LongTensor(input_type), "attention_mask":torch.LongTensor(input_mask), "targets":targets}
+
+
+def get_batch(data_dict, pad_idx, cls_idx, sep_idx, max_query_len=200, max_passage_len=40, batch_size=1, world_size=4):
+
+    for batch in _batch(data_dict, pad_idx, cls_idx, sep_idx, batch_size*world_size, max_query_len, max_passage_len):
+        yield batch
+
+
+
+
